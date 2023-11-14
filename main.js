36e70d37e03941299b138fd1494c286a61fd0807
Jaehyuksssss <ljh95310@naver.com>
2023-11-14 16:36:54 +0900

    ADD: list up

--- main.js
+++ main.js
@@ -1,33 +1,140 @@
 const todoInput = document.querySelector(".todo-input");
-const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+const submitBtn = document.getElementById("submitbtn");
+const leftItems = document.querySelector(".left-items");
+const showAllBtn = document.querySelector(".show-all-btn");
+const showActiveBtn = document.querySelector(".show-active-btn");
+const showCompletedBtn = document.querySelector(".show-completed-btn");
+const clearCompletedBtn = document.querySelector(".clear-completed-btn");
 
 let todos = [];
-let id = 0;
-console.log(todos);
-const init = () => {
-  todoInput.addEventListener("keypress", (e) => {
-    if (e.keyCode === 13) {
-      addTodo();
-    }
+
+function loadStorage() {
+  const storedTodos = localStorage.getItem("todos");
+  console.log(storedTodos);
+  if (storedTodos) {
+    todos = JSON.parse(storedTodos);
+    renderTodos();
+  }
+}
+
+function saveToStorage() {
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function createList() {
+  const todoText = todoInput.value.trim();
+
+  if (todoText !== "") {
+    const todoItem = {
+      id: Date.now(),
+      text: todoText,
+      completed: false,
+    };
+
+    todos.push(todoItem);
+    saveToStorage();
+    renderTodos();
+    todoInput.value = "";
+  }
+}
+
+function renderTodos() {
+  todoList.innerHTML = "";
+
+  todos.forEach((todoItem) => {
+    const li = document.createElement("li");
+    li.style.display = "flex";
+    li.setAttribute("data-id", todoItem.id);
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = todoItem.completed;
+    checkbox.addEventListener("change", toggleTodo);
+
+    const text = document.createElement("span");
+    text.textContent = todoItem.text;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "삭제";
+    deleteBtn.addEventListener("click", deleteTodo);
+
+    li.appendChild(checkbox);
+    li.appendChild(text);
+    li.appendChild(deleteBtn);
+
+    todoList.appendChild(li);
   });
-};
 
-const setTodos = (newTodos) => {
-  todos = newTodos;
-};
+  updateLeftItems();
+}
 
-const getAllTodos = () => {
-  return todos;
-};
+function toggleTodo() {
+  const todoId = parseInt(this.parentNode.getAttribute("data-id"));
+  const todoIndex = todos.findIndex((todo) => todo.id === todoId);
+  todos[todoIndex].completed = !todos[todoIndex].completed;
+  saveToStorage();
+  renderTodos();
+}
 
-const appendTodos = (text) => {
-  const newId = id++;
-  const newTodos = getAllTodos().concat({
-    id: newId,
-    isCompleted: false,
-    content: text,
+function deleteTodo() {
+  const todoId = parseInt(this.parentNode.getAttribute("data-id"));
+  todos = todos.filter((todo) => todo.id !== todoId);
+  saveToStorage();
+  renderTodos();
+}
+
+function updateLeftItems() {
+  const count = todos.filter((todo) => !todo.completed).length;
+  leftItems.textContent = `${count}개의 할 일이 남았어요`;
+}
+
+submitBtn.addEventListener("click", createList);
+showAllBtn.addEventListener("click", () => filterTodos("all"));
+showActiveBtn.addEventListener("click", () => filterTodos("active"));
+showCompletedBtn.addEventListener("click", () => filterTodos("completed"));
+clearCompletedBtn.addEventListener("click", clearCompleted);
+
+function filterTodos(type) {
+  const filteredTodos =
+    type === "all"
+      ? todos
+      : type === "active"
+      ? todos.filter((todo) => !todo.completed)
+      : todos.filter((todo) => todo.completed);
+
+  todoList.innerHTML = "";
+  filteredTodos.forEach((todoItem) => {
+    const li = document.createElement("li");
+    li.setAttribute("data-id", todoItem.id);
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = todoItem.completed;
+    checkbox.addEventListener("change", toggleTodo);
+
+    const text = document.createElement("span");
+    text.textContent = todoItem.text;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.addEventListener("click", deleteTodo);
+
+    li.appendChild(checkbox);
+    li.appendChild(text);
+    li.appendChild(deleteBtn);
+
+    todoList.appendChild(li);
   });
-  setTodos(newTodos);
-  paintTodos();
-};
+}
+
+function clearCompleted() {
+  todos = todos.filter((todo) => !todo.completed);
+  saveToStorage();
+  renderTodos();
+}
+
+function setting() {
+  loadStorage();
+}
+setting();
